e65fc3966d04bf4c1e77aa857f55bd58e13a5d9b
Hunter  Quant <hunterdquant@gmail.com>
2016-03-30 07:57:30 -0400

    Added initial lab4 files.

---
+++ lab4/lab4.js
@@ -0,0 +1,53 @@
+// Name:
+
+var gl;
+var numVertices;
+var numTriangles;
+
+function initGL(){
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    
+    gl.enable(gl.DEPTH_TEST);
+    gl.viewport( 0, 0, 512, 512 );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    
+    var myShaderProgram = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( myShaderProgram );
+
+    
+    numVertices = 1738;
+    numTriangles = 3170;
+    vertices = getVertices(); // vertices and faces are defined in object.js
+    indexList = getFaces();
+    
+    
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexList), gl.STATIC_DRAW);
+    
+    var verticesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    
+    var vertexPosition = gl.getAttribLocation(myShaderProgram,"vertexPosition");
+    gl.vertexAttribPointer( vertexPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vertexPosition );
+    
+    // Insert your code here
+     
+    //
+
+    drawObject();
+
+};
+
+
+function drawObject() {
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    gl.drawElements( gl.TRIANGLES, 3 * numTriangles, gl.UNSIGNED_SHORT, 0 )
+}
+
+
